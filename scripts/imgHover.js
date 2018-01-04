063bf1b4de9bff5b27817b50c22d5875a6431896
Chen <359455492@qq.com>
2018-01-04 09:07:44 +0800

    图片蒙版bug
    图片未加载完成时不能获取宽高，改成css设置宽高

--- scripts/imgHover.js
+++ scripts/imgHover.js
@@ -1,12 +1,11 @@
 $(function(){
 	$("#jnBrandList li").each(function(index) {
-		var $img = $(this).find("img");
-		var img_width = $img.width();
-		var img_height = $img.height();
-		var imgOver = "<span class='imgMask' style='width:"+img_width+"px;height:"+img_height+ "px;position:absolute; left:5px; top:0;'></span>";
-		$(this).find("a").append(imgOver);
+		var imgOver = "<span class='imgMask'></span>";
+		$(this).children("a").append(imgOver);
 	});
-	$("#jnBrandList .imgMask").hover(function(){
+	$("#jnBrandList").on('mouseenter','.imgMask',function(){
+		$(this).toggleClass("imgOver");
+	}).on('mouseleave','.imgMask',function(){
 		$(this).toggleClass("imgOver");
 	});
 	
